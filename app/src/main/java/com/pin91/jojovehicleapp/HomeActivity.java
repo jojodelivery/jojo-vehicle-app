cc9d0c633d02a961f0570abb438810eef053ad73
server@jojodelivery.com <jojodelivery123>
2015-11-18 12:19:34 +0530

    Initial Set up

---
+++ app/src/main/java/com/pin91/jojovehicleapp/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.pin91.jojovehicleapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
